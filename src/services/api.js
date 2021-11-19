5491d8d6f23396a4f58bb98876f6d9be71583497
gemaquejr <gemaquejr@hotmail.com>
2021-11-18 21:54:20 -0300

    req 1 Part of the Task

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,12 @@
 export async function getCategories() {
-  // Implemente aqui
+  return fetch('https://api.mercadolibre.com/sites/MLB/categories')
+    .then((data) => data.json())
+    .then((jsonBody) => {
+      console.log(jsonBody);
+    });
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+
 }
