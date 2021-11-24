6fc32868db46ebeae9e5f1fcd3938684e62b013a
severo <oliveira.severo@gmail.com>
2021-11-24 09:42:53 -0300

    CSS inicial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Content from './components/Content';
+import './App.css';
 
 class App extends React.Component {
   render() {
