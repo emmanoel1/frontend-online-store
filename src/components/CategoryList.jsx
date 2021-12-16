e366d1aaf15dc1cc449ca5b6459ceb7959616ab1
severo <oliveira.severo@gmail.com>
2021-12-15 21:36:46 -0300

    Refatoracao do arquivo categoryList

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -9,9 +9,7 @@ class CategoryList extends React.Component {
     super(props);
     this.state = {
       categories: [],
-      // searchProductsFromCategory: [],
     };
-    // this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -22,46 +20,25 @@ class CategoryList extends React.Component {
     });
   }
 
-  // handleChange(event) {
-  //   // console.log('event_target: ', event.target);
-  //   const { id, value } = event.target;
-  //   // console.log(id);
-  //   console.log(value);
-  //   getProductsFromCategoryAndQuery(id, '').then((query) => {
-  //     this.setState({ searchProductsFromCategory: query.results });
-  //   });
-  // }
-
   render() {
     const { categories } = this.state;
     const { onClickedBtn } = this.props;
 
     return (
-      <>
-        <div className="content_categories">
-          {categories.map((category) => (
-            <label key={ category.id } htmlFor={ category.id } data-testid="category">
-              <input
-                type="radio"
-                name="category"
-                value={ category.name }
-                id={ category.id }
-                onChange={ onClickedBtn } // Requisito 6 - Função para chamar a categoria
-              />
-              { category.name }
-            </label>
-          ))}
-        </div>
-        {/* <div className="products-results">
-          {searchProductsFromCategory.map((product) => (
-            <div key={ product.id } data-testid="product">
-              <h2>{ product.title }</h2>
-              <img src={ product.thumbnail } alt={ product.title } />
-              <h2>{product.price}</h2>
-            </div>
-          ))}
-        </div> */}
-      </>
+      <div className="content_categories">
+        {categories.map((category) => (
+          <label key={ category.id } htmlFor={ category.id } data-testid="category">
+            <input
+              type="radio"
+              name="category"
+              value={ category.name }
+              id={ category.id }
+              onChange={ onClickedBtn } // Requisito 6 - Função para chamar a categoria
+            />
+            { category.name }
+          </label>
+        ))}
+      </div>
     );
   }
 }
