1e5535b2fe8062c11ea08573be5863b526d9dab4
emmanoel <emmanoelporto@gmail.com>
2021-12-18 12:42:00 -0300

    Requisito 9 Pronto

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -1,18 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { getProductsFromProductID } from '../../services/api';
 
 class Details extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      itemOnCart: [],
+    };
+    this.addBtnFn = this.addBtnFn.bind(this);
+  }
+
+  async addBtnFn({ target }) {
+    const productId = target.value;
+    const fetchProduct = await getProductsFromProductID(productId);
+    const { itemOnCart } = this.state;
+    const { location } = this.props;
+    this.setState({
+      itemOnCart: [...itemOnCart, ...location.state.cartProducts, fetchProduct],
+    });
+  }
+
   render() {
     const { location } = this.props;
     const products = [location.state.products];
+    const { itemOnCart } = this.state;
 
     return (
       <div>
         <Link
+          data-testid="shopping-cart-button"
           to={ {
             pathname: '/ShoppingCartPage',
-            state: { product: location.state.cartProducts },
+            state: { product: itemOnCart },
           } }
         >
           <div className="cart_button">
@@ -29,6 +50,15 @@ class Details extends React.Component {
               <span>R$</span>
               {product.price}
             </p>
+            <button
+              className="button is-primary is-small"
+              data-testid="product-detail-add-to-cart"
+              type="submit"
+              value={ product.id }
+              onClick={ this.addBtnFn }
+            >
+              Adicionar Ao Carrinho
+            </button>
           </div>
         ))}
       </div>
